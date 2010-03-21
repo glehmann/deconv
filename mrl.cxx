64677bbfe524e1e97bf7c9f22075c52d273c9428
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2010-03-22 00:56:18 +0100

    use the new SmoothingPeriod attribute instead of the itk::Command management
    Ignore-this: 792d73a1909d27e4caef666b9a7b992c
    
    darcs-hash:20100321235618-2fc9d-508649299394b8a4c8a5b97fb4e26800ebbcb9c6.gz

--- mrl.cxx
+++ mrl.cxx
@@ -7,43 +7,43 @@
 #include "itkRichardsonLucyDeconvolutionImageFilter.h"
 #include "itkMedianImageFilter.h"
 #include "itkTestingMacros.h"
-#include "itkCommand.h"
-
-void disableReg(itk::Object* object, const itk::EventObject &, void*)
-{
-  // the same typedefs than in the main function - should be done in a nicer way
-  const int                 Dimension = 2;
-  typedef unsigned char     PixelType;
-
-  typedef itk::Image< PixelType, Dimension >                   ImageType;
-  typedef itk::RichardsonLucyDeconvolutionImageFilter< ImageType, ImageType > FilterType;
-  typedef itk::MedianImageFilter< FilterType::InternalImageType,
-    FilterType::InternalImageType >                      MedianType;
-  
-  // real stuff begins here
-  // get the LR filter and the median filter
-  FilterType * filter = dynamic_cast< FilterType * >( object );
-  MedianType * median = dynamic_cast< MedianType * >( filter->GetSmoothingFilter() );
-
-  // set half of the slice number as radius
-  MedianType::InputSizeType radius;
-  if( filter->GetIteration() % 10 == 0 )
-    {
-    radius.Fill( 1 );
-    }
-  else
-    {
-    radius.Fill( 0 );
-    }  
-  median->SetRadius( radius );
-}
+// #include "itkCommand.h"
+// 
+// void disableReg(itk::Object* object, const itk::EventObject &, void*)
+// {
+//   // the same typedefs than in the main function - should be done in a nicer way
+//   const int                 Dimension = 2;
+//   typedef unsigned char     PixelType;
+// 
+//   typedef itk::Image< PixelType, Dimension >                   ImageType;
+//   typedef itk::RichardsonLucyDeconvolutionImageFilter< ImageType, ImageType > FilterType;
+//   typedef itk::MedianImageFilter< FilterType::InternalImageType,
+//     FilterType::InternalImageType >                      MedianType;
+//   
+//   // real stuff begins here
+//   // get the LR filter and the median filter
+//   FilterType * filter = dynamic_cast< FilterType * >( object );
+//   MedianType * median = dynamic_cast< MedianType * >( filter->GetSmoothingFilter() );
+// 
+//   // set half of the slice number as radius
+//   MedianType::InputSizeType radius;
+//   if( filter->GetIteration() % 10 == 0 )
+//     {
+//     radius.Fill( 1 );
+//     }
+//   else
+//     {
+//     radius.Fill( 0 );
+//     }  
+//   median->SetRadius( radius );
+// }
 
 int main(int argc, char * argv[])
 {
 
   if( argc < 4 )
     {
-    std::cerr << "usage: " << argv[0] << " intput kernel output [nbIt [th [method [gpf]]]]" << std::endl;
+    std::cerr << "usage: " << argv[0] << " intput kernel output [nbIt [th [period [method [gpf]]]]]" << std::endl;
     std::cerr << " input: the input image" << std::endl;
     std::cerr << " output: the output image" << std::endl;
     std::cerr << " method: padding method - 1: zero flux, 2: zero, 3: mirror, 4: wrap" << std::endl;
@@ -81,14 +81,15 @@ int main(int argc, char * argv[])
   TEST_SET_GET_VALUE( 10, conv->GetNumberOfIterations() );
   TEST_SET_GET_VALUE( 0.0, conv->GetRelativeChangeThreshold() );
   TEST_SET_GET_VALUE( NULL, conv->GetSmoothingFilter() );
+  TEST_SET_GET_VALUE( 1, conv->GetSmoothingPeriod() );
   
   typedef itk::MedianImageFilter< FFTConvolutionType::InternalImageType, FFTConvolutionType::InternalImageType > InternalMedianType;
   InternalMedianType::Pointer imedian = InternalMedianType::New();
   conv->SetSmoothingFilter( imedian );
 
-  itk::CStyleCommand::Pointer command = itk::CStyleCommand::New();
-  command->SetCallback( *disableReg );
-  conv->AddObserver( itk::IterationEvent(), command );
+//  itk::CStyleCommand::Pointer command = itk::CStyleCommand::New();
+//  command->SetCallback( *disableReg );
+//  conv->AddObserver( itk::IterationEvent(), command );
 
   if( argc >= 5 )
     {
@@ -104,14 +105,20 @@ int main(int argc, char * argv[])
 
   if( argc >= 7 )
     {
-    conv->SetPadMethod( atoi(argv[6]) );
-    TEST_SET_GET_VALUE( atoi(argv[6]), conv->GetPadMethod() );
+    conv->SetSmoothingPeriod( atoi(argv[6]) );
+    TEST_SET_GET_VALUE( atoi(argv[6]), conv->GetSmoothingPeriod() );
     }
 
   if( argc >= 8 )
     {
-    conv->SetGreatestPrimeFactor( atoi(argv[7]) );
-    TEST_SET_GET_VALUE( atoi(argv[7]), conv->GetGreatestPrimeFactor() );
+    conv->SetPadMethod( atoi(argv[7]) );
+    TEST_SET_GET_VALUE( atoi(argv[7]), conv->GetPadMethod() );
+    }
+
+  if( argc >= 9 )
+    {
+    conv->SetGreatestPrimeFactor( atoi(argv[8]) );
+    TEST_SET_GET_VALUE( atoi(argv[8]), conv->GetGreatestPrimeFactor() );
     }
   itk::SimpleFilterWatcher watcher_norm(conv, "conv");
 
